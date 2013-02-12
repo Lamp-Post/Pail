0a2acc7a6183cbfd6ef192bf67761731266d5171
feildmaster <admin@feildmaster.com>
2013-02-11 20:24:58 -0600

    Strip ansi color from pail log

--- src/main/java/me/escapeNT/pail/PailLogHandler.java
+++ src/main/java/me/escapeNT/pail/PailLogHandler.java
@@ -1,4 +1,3 @@
-
 package me.escapeNT.pail;
 
 import java.awt.Color;
@@ -7,33 +6,34 @@ import java.util.Date;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.LogRecord;
+import java.util.regex.Pattern;
 import javax.swing.SwingUtilities;
 import javax.swing.UIManager;
-
 import me.escapeNT.pail.Util.ScrollableTextArea;
 import me.escapeNT.pail.Util.Util;
-
 import org.bukkit.Bukkit;
 
 /**
  * Log Handler to print the console output to the GUI
+ *
  * @author escapeNT
  */
 public class PailLogHandler extends Handler {
-
     private ScrollableTextArea output;
-
-    public static String lastMessage = "";
+    private static Pattern pattern = Pattern.compile("\\x1B\\[([0-9]{1,2}(;[0-9]{1,2})?)?[m|K]");
+    private static String lastMessage = "";
 
     /**
      * Constructs a new log handler using the specified text area for output.
+     *
      * @param output The JTextArea to write the log data to.
      */
     public PailLogHandler() {
         output = new ScrollableTextArea();
         output.setAutoscrolls(true);
 
-        Bukkit.getServer().getScheduler().scheduleAsyncRepeatingTask(Util.getPlugin(), new Runnable() {
+        // TODO: ..........
+        Bukkit.getServer().getScheduler().runTaskTimerAsynchronously(Util.getPlugin(), new Runnable() {
             public void run() {
                 PailLogHandler.lastMessage = "";
             }
@@ -41,64 +41,48 @@ public class PailLogHandler extends Handler {
     }
 
     public synchronized void publish(final LogRecord record) {
-
         SwingUtilities.invokeLater(new Runnable() {
             public void run() {
+                String message = pattern.matcher(record.getMessage()).replaceAll("");
 
-                char[] c = record.getMessage().toCharArray();
-                StringBuilder message = new StringBuilder(record.getMessage());
-                int off = 0;
-                try {
-                    for(int i = 0; i < c.length; i++) {
-                        if(c[i] == '[' && Character.isDigit(c[i+1])) {
-                            if(Character.isDigit(c[i+2]) && c[i+3] == 'm') {
-                                message.delete(i - (off + 1), i + 4 - off);
-                                off += 5;
-                            } else if(c[i+2] == 'm') {
-                                message.delete(i - (off + 1), i + 3 - off);
-                                off += 4;
-                            }
-                        }
-                    }
-                } catch(IndexOutOfBoundsException e) {}
-
-                if(message.toString().equals(PailLogHandler.lastMessage)) {
+                if (message.equals(PailLogHandler.lastMessage)) {
                     return;
-                } else {
-                    PailLogHandler.lastMessage = message.toString();
                 }
+                PailLogHandler.lastMessage = message;
 
                 output.append(Color.GRAY, true, new SimpleDateFormat("hh:mm a").format(new Date(record.getMillis())));
 
                 Color color = Color.BLACK;
                 Level lv = record.getLevel();
-                if(lv == Level.INFO) {
+                if (lv == Level.INFO) {
                     color = Color.BLUE;
-                } else if(lv == Level.WARNING) {
+                } else if (lv == Level.WARNING) {
                     color = Color.ORANGE;
-                } else if(lv == Level.SEVERE) {
+                } else if (lv == Level.SEVERE) {
                     color = Color.RED;
                 }
 
                 output.append(color, " [" + record.getLevel().toString() + "] ");
 
-                if(UIManager.getLookAndFeel().getName().equals("HiFi")) {
+                if (UIManager.getLookAndFeel().getName().equals("HiFi")) {
                     color = Color.WHITE;
                 } else {
                     color = Color.BLACK;
                 }
-                for(String s : message.toString().trim().split(" ")) {
+                for (String s : message.toString().trim().split(" ")) {
                     output.append(color, (((s.startsWith("[") && s.contains("]"))
-                        || (s.startsWith("<") && s.contains(">")))), s.trim() + " ");
+                            || (s.startsWith("<") && s.contains(">")))), s.trim() + " ");
                 }
                 output.append(color, "\n");
             }
         });
     }
 
-    public void flush() {}
+    public void flush() {
+    }
 
-    public void close() throws SecurityException {}
+    public void close() throws SecurityException {
+    }
 
     public ScrollableTextArea getTextArea() {
         return output;
